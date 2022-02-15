944e4793d3394144ff2a2ede9fb60b8c35e7322e
ext-deviget-pavlo.pylypenko <pavlo.pylypenko@ringteam.com>
2022-02-15 10:58:02 +0200

    Home work 12

--- src/StreamTask.java
+++ src/StreamTask.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
+
 public class StreamTask {
 
-    public static int getSumSquareEven(int[] numbers) {
-        // TODO replace return with your code
-        return 0;
-    }
+  public static int getSumSquareEven(int[] numbers) {
+    int res = Arrays.stream(numbers).filter(n -> n % 2 == 0).reduce(0, (acc, elem) -> acc + elem * elem);
+    return res;
+  }
 
-    public static int[] getSortedOddsArray(int[] numbers) {
-        // TODO replace return with your code
-        return new int[]{};
-    }
+  public static int[] getSortedOddsArray(int[] numbers) {
+    int[] res = Arrays.stream(numbers).sorted().filter(n -> n % 2 != 0).toArray();
+    return res;
+  }
 }
--- src/StreamTaskTest.java
+++ src/StreamTaskTest.java
@@ -5,51 +5,69 @@ import static org.testng.Assert.assertEquals;
 
 public class StreamTaskTest {
 
-    @DataProvider(name = "numbers")
-    public static Object[][] numbers() {
-        return new Object[][]{
-                {
-                        new int[]{1, 2, 3, 4, 5},
-                        20
-                },
-                {
-                        new int[]{-5, -4, 11, 6, 5},
-                        52
-                },
-                {
-                        new int[]{13, -2, 33, -4, 5},
-                        20
-                }
-                // TODO add 2 more test data here
-        };
-    }
+  @DataProvider(name = "numbers")
+  public static Object[][] numbers() {
+    return new Object[][]{
+        {
+            new int[]{1, 2, 3, 4, 5},
+            20
+        },
+        {
+            new int[]{-5, -4, 11, 6, 5},
+            52
+        },
+        {
+            new int[]{13, -2, 33, -4, 5},
+            20
+        },
+        {
+            new int[]{0, 2, -3, -6, 11},
+            40
+        },
+        {
+            new int[]{130, -200, 330, -40, 50, 66},
+            174256
+        },
+        {
+            new int[]{},
+            0
+        }
+    };
+  }
 
-    @DataProvider(name = "arrays")
-    public static Object[][] arrays() {
-        return new Object[][]{
-                {
-                        new int[]{1, 4, 3, 5, 2},
-                        new int[]{1, 3, 5}
-                },
-                {
-                        new int[]{5, -3, 11, -5},
-                        new int[]{-5, -3, 5, 11}
-                },
-                {
-                        new int[]{130, -200, 330, -40, 50, 66},
-                        new int[]{}
-                }
-                // TODO add 2 more test data here
-        };
-    }
+  @DataProvider(name = "arrays")
+  public static Object[][] arrays() {
+    return new Object[][]{
+        {
+            new int[]{1, 4, 3, 5, 2},
+            new int[]{1, 3, 5}
+        },
+        {
+            new int[]{5, -3, 11, -5},
+            new int[]{-5, -3, 5, 11}
+        },
+        {
+            new int[]{130, -200, 330, -40, 50, 66},
+            new int[]{}
+        },
+        {
+            new int[]{0, 5, -4, 3, -2, 1, -1},
+            new int[]{-1, 1, 3, 5}
+        },
+        {
+            new int[]{5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5},
+            new int[]{-5, -3, -1, 1, 3, 5}
+        }
+    };
+  }
 
-    @Test(dataProvider = "numbers")
-    public void testSumSquareEven(int[] numbers, int expectedResult) {
-        assertEquals(StreamTask.getSumSquareEven(numbers), expectedResult, "Max value is incorrect");
-    }
+  @Test(dataProvider = "numbers")
+  public void testSumSquareEven(int[] numbers, int expectedResult) {
+    assertEquals(StreamTask.getSumSquareEven(numbers), expectedResult, "Max value is incorrect");
+  }
 
-    @Test(dataProvider = "arrays")
-    public void getSortedOddsArray(int[] numbers, int[] expectedResult) {
-        assertEquals(StreamTask.getSortedOddsArray(numbers), expectedResult, "Array is incorrect");
-    }
+  @Test(dataProvider = "arrays")
+  public void getSortedOddsArray(int[] numbers, int[] expectedResult) {
+    assertEquals(StreamTask.getSortedOddsArray(numbers), expectedResult, "Array is incorrect");
+  }
 }
